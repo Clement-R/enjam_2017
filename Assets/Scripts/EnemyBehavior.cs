2bcbe83d342194a2564b528cc9a5eff0c02fa178
Clement-R <rondeau.clement@gmail.com>
2017-10-07 10:56:43 +0200

    Setup scene ratio and first test assets

---
+++ Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+	void Start () {
+        // EventManager.StartListening("playerMove", OnPlayerMove);
+    }
+	
+	void Update () {
+		
+	}
+}
